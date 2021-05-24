e7bf907c8881b8e6d3109916055667ec6fd54ae4
Testuser001-debug <84234115+Testuser001-debug@users.noreply.github.com>
2021-05-24 15:40:43 +0100

    NEW BOBUX GENERATOR 2077

---
+++ generators/bobux generator 2077.cs
@@ -0,0 +1,11 @@
+using System;
+// yeah, no one carez
+namespace Bobux generator 2077
+{
+ Class Program
+ {
+ static void Main(string[] args)
+ {
+  Console.ReadLine();
+Console.WriteLine('GENERATING BOBUX.... DONE. :)')
+// end
