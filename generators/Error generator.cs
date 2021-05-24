7c35451fa035f3a7a8e66094819817589ed88abd
Testuser001-debug <84234115+Testuser001-debug@users.noreply.github.com>
2021-05-24 15:34:12 +0100

    if a error found use this

---
+++ generators/Error generator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Namespace Fix error generata
+{
+  class Program
+  {
+  static void Main(string[] args)
+  {
+  string phrase = "hello";
+
+
+    Console.WriteLine('Fixing errors');
+    
+   Console.ReadLine();
+   }
+ }
+}
