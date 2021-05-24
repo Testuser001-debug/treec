5162fd9cda0259d5bbe1b5c7b71a4b211d715bb1
Testuser001-debug <84234115+Testuser001-debug@users.noreply.github.com>
2021-05-24 15:16:08 +0100

    Edit file just to prevent our confusion of 'Hello C#'!

--- control.cs
+++ control.cs
@@ -1,1 +1,10 @@
-Console.writeline('Hello World!')
+Console.Writeline('Injecting!') >> true()
+// remove >> as a example
+if false
+Console.Writeline('Failed due to a false script injected, please remove 'if false' as a example!')
+if true
+Console.Writeline('... trying again...')
+if false
+if (CR > LF*2) then "Mac" 
+else if (LF > CR*2) then "Unix"
+else "Windows"
