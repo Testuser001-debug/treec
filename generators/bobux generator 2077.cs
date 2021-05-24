43ab8ca13913ec562546d86e03d7409c27a7d7c7
Testuser001-debug <84234115+Testuser001-debug@users.noreply.github.com>
2021-05-24 15:41:47 +0100

    errors, errors everywhere.

--- generators/bobux generator 2077.cs
+++ generators/bobux generator 2077.cs
@@ -1,11 +1,4 @@
 using System;
-// yeah, no one carez
-namespace Bobux generator 2077
-{
- Class Program
- {
- static void Main(string[] args)
- {
-  Console.ReadLine();
-Console.WriteLine('GENERATING BOBUX.... DONE. :)')
-// end
+Console.WriteLine('bobux generated already')
+if false
+Console.WriteLine('GENERATING BOBUX HAVE COMPLETE')
